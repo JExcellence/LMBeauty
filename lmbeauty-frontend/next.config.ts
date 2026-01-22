ee3e2ed2eeeb81d496973c6eaa8a0c9975de0585
JExcellence | Justin <justin.eiletz@outlook.com>
2026-01-22 17:11:55 +0100

    Initialize Project LM Beauty

--- lmbeauty-frontend/next.config.ts
+++ lmbeauty-frontend/next.config.ts
@@ -29,6 +29,16 @@ const nextConfig: NextConfig = {
                 protocol: 'https',
                 hostname: 'images.unsplash.com',
             },
+            {
+                protocol: 'https',
+                hostname: 'scontent-*.cdninstagram.com',
+                pathname: '/**',
+            },
+            {
+                protocol: 'https',
+                hostname: '*.cdninstagram.com',
+                pathname: '/**',
+            },
         ],
         unoptimized: false,
     },
